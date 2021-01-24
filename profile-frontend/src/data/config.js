894bb35a320aafd45d19af63bbef064715ea5f77
ajay A <ajaythanjore@gmail.com>
2021-01-24 13:53:09 +0000

    initial1

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Ajay",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Ajay , pursuing  btech in SSN college of engineering!",
+  githubUrl: "https://github.com/ajayannadurai",
+  linkedinUrl: "https://www.linkedin.com/in/ajay-a-19315118a/",
+  cvLink: "https://drive.google.com/file/d/1bgZwfpKd1WsmmVBjRD5sT-s4QnfRG0cP/view?usp=sharing",
+};
